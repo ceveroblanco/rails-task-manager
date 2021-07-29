0b7e0662ee993221953b07cc8946ff5a1b1213e6
jose rivera <riverajoseluciano@gmail.com>
2021-07-29 15:05:35 -0400

    ejercicio listo, solo falta embellecer

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+
   end
   
 
@@ -18,13 +20,33 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    
+  end
+
+  def update
+   
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+   
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
   private
 
-  def restaurant_params
+  def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
 
-  def edit
+  def set_task
     @task = Task.find(params[:id])
   end
+
+
 end
